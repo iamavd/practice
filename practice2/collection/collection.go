50b426559d5734fe7e03b22e5c1a0aac67702727
iamavd <a.m.avd@mail.ru>
2019-10-31 15:02:42 +0300

    Realised remove method for linked list

--- practice2/collection/collection.go
+++ practice2/collection/collection.go
@@ -5,51 +5,78 @@ import (
 	"strconv"
 )
 
+const sep = " "
+
 type List struct {
-	Length int
-	First  *Element
-	Last   *Element
+	first  *Element
+	last   *Element
 }
 
 type Element struct {
-	Value string
-	Next  *Element
-	Prev  *Element
+	value string
+	next  *Element
+	prev  *Element
 }
 
 func (c *List) Print() {
-	res := c.First
-	for i := 0; i < 10; i++ { //FIXME
-		fmt.Print(res.Next.Value, " ")
-		res = res.Next
+	for elem:=c.first; elem.next != nil; elem=elem.next { 
+		fmt.Print(elem.next.value, sep)
 	}
+	fmt.Println("")
 }
 
 func (c *List) Add(i int) {
 	e := &Element{
-		Value: strconv.Itoa(i),
+		value: strconv.Itoa(i),
 	}
 
-	if c.First == nil {
-		c.First = e
-		c.Last = e
+	if c.first == nil {
+		c.first = e
+		c.last = e
 	} else {
-		last := c.Last
-		last.Next = e
-		c.Last = e
-		c.Last.Prev = last
+		last := c.last
+		last.next = e
+		c.last = e
+		c.last.prev = last
 	}
-	c.Length++
 }
 
 func (c *List) Get(i int) *Element {
-	res := c.First
+	res := c.first
 	for a := 0; a < i; a++ {
-		res = res.Next
+		res = res.next
 	}
 	return res
 }
 
-func (c *List) Remove(i int) {
-	c.Get(i).Value = "1111" //.Value = "-" //c.Get(i).Prev.Prev
+func (c *List) Remove(index int) {
+	var elem *Element
+	elem = c.Get(index)
+
+	if elem.prev != nil {
+		elem.prev.next = elem.next
+	} else{
+		c.first = elem.next
+	}
+	elem = nil
+}
+
+func (c *List) Length() int {
+	i:=0
+	for elem:=c.first; elem != nil; elem = elem.next {
+		i++
+	}
+	return i
+}
+
+func (c* List) First() string {
+	return c.first.value
+}
+
+func (c* List) Last() string {
+	return c.last.value
+}
+
+func (e* Element) Value() string {
+	return string(e.value)
 }
--- practice2/main.go
+++ practice2/main.go
@@ -6,32 +6,17 @@ import (
 )
 
 func main() {
-
 	col := collection.List{}
 
 	for i := 0; i < 10; i++ {
 		col.Add(i)
-		fmt.Println(col.Last)
 	}
-	col.Add(12)
 
 	col.Print()
-	fmt.Println("")
-	fmt.Println(col.Get(1).Value)
-	fmt.Println("")
-
-	//	col.Print()
 	col.Remove(4)
 	col.Print()
-	//	col.Print()
-	//	col.Add(33)
-	//	col.Print()
-	//col.Print()
-	//col.Remove(4)
-	//col.Print()
-	//fmt.Println(col.Get(3))
-	//fmt.Println(col.First())
-	//fmt.Println(col.Last())
-	//	fmt.Println(col.Length())
-
+	fmt.Println(col.Get(3).Value())
+	fmt.Println(col.First())
+	fmt.Println(col.Last())
+	fmt.Println(col.Length())
 }
