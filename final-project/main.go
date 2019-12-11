608c94de6ae900070d2d0130e02e96c44aa121f8
iamavd <a.m.avd@mail.ru>
2019-12-11 16:15:19 +0300

    Added simple handler for test

--- final-project/main.go
+++ final-project/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"final-project/handler"
+	"log"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Time to start")
+func main() {
+	http.HandleFunc("/", handler.Sayhello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
