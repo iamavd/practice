e23a3e78f08de944f4fb41b46ae9aaff1cc71750
iamavd <a.m.avd@mail.ru>
2019-12-16 15:51:16 +0300

    Added mongo database initilization

--- final-project/main.go
+++ final-project/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"context"
 	"final-project/handler"
+	"final-project/mongodb"
 	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+const (
+	dbAddress = "127.0.0.1:27017"
+	dbName    = "MyDataBase"
+)
+
+func initMongoDB() *mongo.Database {
+
+	var conf mongodb.Config
+	conf.Address = dbAddress
+	conf.Database = dbName
+	db, err := mongodb.Init(context.Background(), &conf)
+	if err != nil {
+		log.Panicf("could not connect to the database: %v", err)
+	}
+	return db
+}
+
 func main() {
+	db := initMongoDB()
+	defer func() {
+		if err := db.Client().Disconnect(context.Background()); err != nil {
+			log.Printf("could not disconnect from database: %v", err)
+		}
+	}()
+
 	http.HandleFunc("/", handler.Sayhello)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+
 }
--- final-project/mongodb/config.go
+++ final-project/mongodb/config.go
@@ -2,13 +2,52 @@ package mongodb
 
 import (
 	"context"
+	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
-	"time"
 )
 
+type Config struct {
+	Username string
+	Password string
+	Address  string
+	Database string
+}
+
+func Init(ctx context.Context, cfg *Config) (*mongo.Database, error) {
+	opts, err := createOptions(cfg)
+	if err != nil {
+		return nil, err
+	}
+	c, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+	if err = checkConnection(ctx, c); err != nil {
+		return nil, err
+	}
+	return c.Database(cfg.Database), nil
+}
+
+func createOptions(cfg *Config) (*options.ClientOptions, error) {
+	url := "mongodb://"
+	if cfg.Username != "" {
+		url += cfg.Username
+	}
+	if cfg.Password != "" {
+		url += ":" + cfg.Password + "@"
+	}
+	url += cfg.Address
+	opts := options.Client().ApplyURI(url)
+	if err := opts.Validate(); err != nil {
+		return nil, err
+	}
+	return opts, nil
+}
+
+/*
 func InitDB() {
 	ctx := context.Background()
 
@@ -28,7 +67,7 @@ func InitDB() {
 			log.Fatal(err)
 		}
 	}()
-}
+}*/
 
 func checkConnection(ctx context.Context, c *mongo.Client) error {
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
