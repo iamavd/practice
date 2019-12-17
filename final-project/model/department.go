8c2e13aeb822133abf4d6bb8bdd8f811c898a5f7
iamavd <a.m.avd@mail.ru>
2019-12-17 14:38:27 +0300

    fixed id in department model

--- final-project/model/department.go
+++ final-project/model/department.go
@@ -1,13 +1,12 @@
 package model
 
-//import "github.com/mongodb/mongo-go-driver/bson"
-import "labix.org/v2/mgo/bson"
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 //Department contains the information about departments - name, workers and head of department
 type Department struct {
-	ID          bson.ObjectId `bson:"_id"`
-	Name        string        `bson:"deptName"`
-	Description string        `bson:"description"`
-	Head        bson.ObjectId     `bson:"head"`
-	Employees   []bson.ObjectId   `bson:"employees"`
+	ID          primitive.ObjectID   `bson:"_id"`
+	Name        string               `bson:"deptName"`
+	Description string               `bson:"description"`
+	Head        primitive.ObjectID   `bson:"head"`
+	Employees   []primitive.ObjectID `bson:"employees"`
 }
